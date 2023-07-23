06fb6768966f238b83875f91a37cfa9d781ca229
Sebastian <sebastian.williamson.vega@gmail.com>
2023-07-23 19:04:21 +0200

    New Commit

--- connection.py
+++ connection.py
@@ -1,6 +1,16 @@
 import json
 import requests
+import 
 
-rescuetime = "kPYz"
+rescuetime = ""
 
-def ()
+def rescuetime():
+    # define headers and URL
+    headers = {
+        'Host': 'https://www.rescuetime.com/anapi/data',
+        'format': 'json' 
+    }
+    url = 'https://api.ouraring.com/v1/sleep?start=2022-07-16&end=2022-07-16'
+
+    response = requests.get(url, headers=headers)
+    return response
